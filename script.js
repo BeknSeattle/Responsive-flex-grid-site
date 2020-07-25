a7ec42123f3a49ff76dcd70ef2b7ecf8288ca408
Maria Victoria Reyes <mariavictoriabreyes@gmail.com>
2020-07-24 23:03:45 -0700

    changes in javascript

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
-var _ = require('lodash');
+// var _ = require('lodash');
 
-var array = [1,2,3,4,5,6,7,8];
-console.log('answer', _.without(array, 3))
+// var array = [1,2,3,4,5,6,7,8];
+// console.log('answer', _.without(array, 3))
+
+const boxes = document.querySelectorAll('.box');
+console.log(boxes);
+boxes.forEach(box => {
+    box.classList.toggle('grow');
+    console.log(box);
+});
+
+//navigation
+const section = document.querySelectorAll('section');
+console.log(section);
+section.forEach(element => {
+    let navTitle = element.id;
+    let idTitle = [];
+    idTitle.push(navTitle)
+    console.log(idTitle);
+});
+
+const nav = document.querySelector('ul');
+const navLi = document.createElement('li');
+const a = document.createElement('a');
+a.setAttribute('href', "#");
+navLi.append(a);
+nav.insertAdjacentElement('afterbegin', navLi);
+console.log(nav);
+
